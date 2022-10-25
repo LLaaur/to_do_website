3f01b8d5b8914f969e66723a20d8af6d9f3055b6
Laur <laur_laurentiu2013@yahoo.com>
2022-10-25 19:35:10 +0300

    make toggle dark/light mode functional (not the optimal solution yet)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('wasup');
+const checkbox = document.getElementById('darkmode-toggle');
+const nav = document.querySelector('.navbar');
+const sideMenu = document.querySelector('.side-menu');
+const rightPanel = document.querySelector('.right-panel');
+
+checkbox.addEventListener('input', () => {
+    if (checkbox.checked) {
+        nav.style.backgroundColor = '#202025';
+      nav.style.color = 'white';
+      sideMenu.style.backgroundColor = '#2b2b2e';
+      sideMenu.style.color = 'white';
+      rightPanel.style.backgroundColor = '#1a1a1b';
+      rightPanel.style.color = 'white';
+    } else {
+        nav.style.backgroundColor = '#FAF6F5';
+        nav.style.color = 'black';
+        sideMenu.style.backgroundColor = '#F7F8F3';
+        sideMenu.style.color = 'black';
+        rightPanel.style.backgroundColor = '#f8f3f0';
+        rightPanel.style.color = 'black';
+    }
+  });

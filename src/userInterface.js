95c3617b01b710a25127be38ea958630e264ab0c
Laur <laur_laurentiu2013@yahoo.com>
2022-10-31 17:45:29 +0200

    implement basic ToDoList logic and create module for all the user-interface related events

---
+++ src/userInterface.js
@@ -0,0 +1,55 @@
+function Interface() {
+
+    const checkbox = document.getElementById('darkmode-toggle');
+    const nav = document.querySelector('.navbar');
+    const sideMenu = document.querySelector('.side-menu');
+    const rightPanel = document.querySelector('.right-panel');
+
+    const menuBtn = document.querySelector('.menu-button');
+
+    const addProjectBtn = document.querySelector('.add-project');
+    const addProjectpopup = document.getElementById('add-project-popup');
+
+    const addCancelProject = document.querySelector('.add-or-cancel-buttons');
+
+    const cancelProjectBtn = document.querySelector('.cancel-project-button');
+
+    checkbox.addEventListener('input', () => {
+        if (checkbox.checked) {
+            nav.style.backgroundColor = '#202025';
+            nav.style.color = 'white';
+            sideMenu.style.backgroundColor = '#2b2b2e';
+            sideMenu.style.color = 'white';
+            rightPanel.style.backgroundColor = '#1a1a1b';
+            rightPanel.style.color = 'white';
+        }
+        else {
+            nav.style.backgroundColor = '#FAF6F5';
+            nav.style.color = 'black';
+            sideMenu.style.backgroundColor = '#F7F8F3';
+            sideMenu.style.color = 'black';
+            rightPanel.style.backgroundColor = '#f8f3f0';
+            rightPanel.style.color = 'black';
+        }
+    });
+
+
+    menuBtn.addEventListener('click', () => {
+        sideMenu.classList.toggle('minimize');
+        rightPanel.style.width = '100%';
+    })
+
+    addProjectBtn.addEventListener('click', () => {
+        addProjectpopup.style.display = 'block';
+        addProjectpopup.value = '';
+        addCancelProject.style.display = 'flex';
+    })
+
+    cancelProjectBtn.addEventListener('click', () => {
+        addProjectpopup.style.display = 'none';
+        addCancelProject.style.display = 'none';
+    })
+
+}
+
+export default Interface;
